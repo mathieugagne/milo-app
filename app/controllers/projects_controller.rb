f64e7837d5f7404a7e3f664563a25808894f7b65
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-05-01 09:29:27 -0400

    Add status to project forms

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -55,6 +55,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def project_params
-      params.require(:project).permit(:title, :description)
+      params.require(:project).permit(:title, :description, :status)
     end
 end
