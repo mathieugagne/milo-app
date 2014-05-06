375ca40d74eb09fff7a4840fb04a2aa8a018a635
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-05-06 16:50:48 -0400

    Destroy associated project comments

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   validates :title, presence: true
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 
   enum status: [:pending, :ongoing, :completed, :canceled]
 end
