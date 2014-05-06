b0c9ca53e00408272399501a7522917b46b55dc9
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-05-06 15:25:51 -0400

    Add project status label

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,14 @@
 module ProjectsHelper
+
+  def project_status_label status
+    status_class = case status
+    when 'ongoing' then 'info'
+    when 'completed' then 'success'
+    when 'canceled' then 'warning'
+    else
+      'default'
+    end
+    content_tag(:span, status, class: "label label-#{status_class}")
+  end
+
 end
