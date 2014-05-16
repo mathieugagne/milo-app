b6f6e75bd91654c08dfa3d4006193f57ebdf5ba7
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-05-15 21:26:37 -0400

    Add avatar uploading form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
     raise ActionController::RoutingError.new('Not Found') unless user_signed_in?
     @projects = current_user.projects
   end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to profile_path, notice: 'Profile was successfully updated.'
+    else
+      redirect_to profile_path, alert: 'Oups. Something went wrong.'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users, only: :update
   get '/profile', to: 'users#profile'
 
   get 'comments/create'
