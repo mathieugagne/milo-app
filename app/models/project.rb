6fc3fbd226bc35c827d56dcfe8430687bc73ba54
Mathieu Gagné <mgagne@vigilantglobal.com>
2014-05-16 10:24:29 -0400

    Index projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,18 @@ class Project < ActiveRecord::Base
   enum status: [:pending, :ongoing, :completed, :canceled]
 
   has_and_belongs_to_many :users
+
+  searchable do
+    text :title, :description, :status
+    text :comments do
+      comments.map(&:text).join(' ')
+    end
+    text :assignees do
+      users.map(&:email).join(' ')
+    end
+    string :title
+    string :description
+    string :status
+    integer :comments_count
+  end
 end
